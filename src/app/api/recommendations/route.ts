ede46de5719b9f80910b99ab85c4f1cbfb3f6cd4
idrischaalel <71793123+chaalelidris@users.noreply.github.com>
2023-07-04 08:57:30 +0100

    Fix: RS with sorted users based on similarity

--- src/app/api/recommendations/route.ts
+++ src/app/api/recommendations/route.ts
@@ -1,8 +1,7 @@
-import prisma from '@/libs/prismadb'; // Import your Prisma client
+import prisma from '@/libs/prismadb';
 import { SafePreferences } from '@/types';
 import { Listing } from '@prisma/client';
 import { NextResponse } from 'next/server';
-
 
 interface User {
     id: string;
@@ -18,8 +17,8 @@ export async function POST(req: Request) {
             select: {
                 id: true,
                 preferences: true,
-                favoriteIds: true
-            }
+                favoriteIds: true,
+            },
         });
 
         if (!user) {
@@ -30,22 +29,21 @@ export async function POST(req: Request) {
             select: {
                 id: true,
                 preferences: true,
-                favoriteIds: true
-            }
+                favoriteIds: true,
+            },
         });
+
         const items = await prisma.listing.findMany({
             include: {
                 images: true,
             },
         });
 
-
-
         // Calculate user-user similarity using cosine similarity
         const userSimilarities = calculateUserSimilarities(users, items);
 
 
-        // Get similar users based on cosine similarity
+        // Get similar users based on cosine similarity | sorted array with similar users ids[...]
         const similarUsers = findSimilarUsers(user.id, userSimilarities);
 
         // Get items liked by similar users and not liked by the target user
@@ -64,40 +62,38 @@ export async function POST(req: Request) {
     }
 }
 
-
-
 type CosineSimilarityMatrix = Record<string, number[]>;
+
 function calculateUserSimilarities(users: User[], items: Listing[]): CosineSimilarityMatrix {
     const userSimilarities: CosineSimilarityMatrix = {};
 
     for (const user of users) {
-
-        const userPreferences = user.preferences; // ["modern","Beach",...]
+        const userPreferences = user.preferences;
         const userVector = getUserVector(userPreferences, items);
-
         userSimilarities[user.id] = [];
 
         for (const otherUser of users) {
             const otherUserPreferences = otherUser.preferences;
             const otherUserVector = getUserVector(otherUserPreferences, items);
-
             const similarity = calculateCosineSimilarity(userVector, otherUserVector);
-
             userSimilarities[user.id].push(similarity);
+
         }
+
     }
 
     return userSimilarities;
 }
 
+
 function getUserVector(preferences: SafePreferences | null, items: Listing[]): number[] {
     const userVector: number[] = [];
 
     for (const item of items) {
-
-        if (preferences?.categories.includes(item.category)
-            && preferences?.location === item.wilayaLocationValue) {
-
+        if (
+            preferences?.categories.includes(item.category) &&
+            preferences?.location === item.wilayaLocationValue
+        ) {
             userVector.push(1);
         } else {
             userVector.push(0);
@@ -107,56 +103,11 @@ function getUserVector(preferences: SafePreferences | null, items: Listing[]): n
     return userVector;
 }
 
-
-function findSimilarUsers(userId: string, userSimilarities: CosineSimilarityMatrix): string[] {
-    const userSimilaritiesArray = Object.entries(userSimilarities);
-    const targetUserSimilarities = userSimilaritiesArray.find(([id]) => id === userId);
-
-    if (!targetUserSimilarities) {
-        return [];
-    }
-
-    const targetUserIndex = userSimilaritiesArray.indexOf(targetUserSimilarities);
-    const targetUserSimilarityScores = targetUserSimilarities[1] as number[];
-    const similarUsers: string[] = [];
-
-    for (let i = 0; i < targetUserSimilarityScores.length; i++) {
-        if (i !== targetUserIndex && targetUserSimilarityScores[i] > 0) {
-            similarUsers.push(userSimilaritiesArray[i][0]);
-        }
-    }
-
-    return similarUsers;
-}
-
-async function findRecommendedItems(
-    user: User,
-    similarUsers: string[],
-    items: Listing[]
-): Promise<Listing[]> {
-    const userFavorites = new Set(user.favoriteIds);
-    const recommendedItems: Listing[] = [];
-    const users = await prisma.user.findMany();
-
-
-    for (const similarUser of similarUsers) {
-        const similarUserFavorites = new Set(users.find((user) => user.id === similarUser)?.favoriteIds || []);
-
-
-        for (const item of items) {
-            if (similarUserFavorites.has(item.id) && !userFavorites.has(item.id)) {
-                recommendedItems.push(item);
-            }
-        }
-    }
-
-    return recommendedItems;
-}
-
-
-
 function calculateCosineSimilarity(vectorA: number[], vectorB: number[]): number {
-    const dotProduct = vectorA.reduce((acc, value, index) => acc + value * vectorB[index], 0);
+    const dotProduct = vectorA.reduce(
+        (acc, value, index) => acc + value * vectorB[index],
+        0
+    );
     const magnitudeA = Math.sqrt(vectorA.reduce((acc, value) => acc + value * value, 0));
     const magnitudeB = Math.sqrt(vectorB.reduce((acc, value) => acc + value * value, 0));
 
@@ -167,5 +118,63 @@ function calculateCosineSimilarity(vectorA: number[], vectorB: number[]): number
     return dotProduct / (magnitudeA * magnitudeB);
 }
 
+function findSimilarUsers(userId: string, userSimilarities: CosineSimilarityMatrix): string[] {
+    const userSimilaritiesArray = Object.entries(userSimilarities);
+  
+    const targetUserSimilarities = userSimilaritiesArray.find(([id]) => id === userId);
+  
+    if (!targetUserSimilarities) {
+      return [];
+    }
+  
+    const targetUserIndex = userSimilaritiesArray.indexOf(targetUserSimilarities);
+    const targetUserSimilarityScores = targetUserSimilarities[1] as number[];
+    const similarUsers: string[] = [];
+  
+    // Create an array of objects with user ID and similarity score
+    const usersWithScores = userSimilaritiesArray.map(([id, similarityScores], index) => ({
+      id,
+      similarityScore: similarityScores[targetUserIndex],
+      index,
+    }));
+  
+    // Sort the array based on similarity score in descending order
+    usersWithScores.sort((a, b) => b.similarityScore - a.similarityScore);
+  
+    // Extract the user IDs in the sorted order
+    for (const user of usersWithScores) {
+      if (user.index !== targetUserIndex && user.similarityScore > 0) {
+        similarUsers.push(user.id);
+      }
+    }
+  
+  
+    return similarUsers;
+  }
+  
+
+async function findRecommendedItems(
+    user: User,
+    similarUsers: string[],
+    items: Listing[]
+): Promise<Listing[]> {
+    const userFavorites = new Set(user.favoriteIds);
+    const recommendedItems: Listing[] = [];
+    const users = await prisma.user.findMany();
+
+    for (const similarUser of similarUsers) {
+        const similarUserFavorites = new Set(
+            users.find((user) => user.id === similarUser)?.favoriteIds || []
+        );
+
+        for (const item of items) {
+            if (similarUserFavorites.has(item.id) && !userFavorites.has(item.id)) {
+                recommendedItems.push(item);
+            }
+        }
+    }
+
+    return recommendedItems;
+}
 
 
