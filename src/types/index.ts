5753bcc11e338f78c1e48b2b94e9fef55e2c5b3d
idris chaalel <idrischaalel8@gmail.com>
2023-12-15 23:18:19 +0100

    update SafeReservation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,41 +1,39 @@
-import { User, Prisma, Reservation, Review, Preference } from '@prisma/client';
+import { User, Prisma, Reservation, Review, Preference } from "@prisma/client";
 
 export type SafeUser = Omit<
   User,
-  'createdAt' | 'updatedAt' | 'emailVerified' | 'walletId'
+  "createdAt" | "updatedAt" | "emailVerified" | "walletId"
 > & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
-  hasWallet: boolean
+  hasWallet: boolean;
   balance: number | null;
 };
 
 export type SafePreferences = Preference;
 
-
-export type SafeReview = Omit<Review,
-  'createdAt' | 'updatedAt'> & {
-    createdAt: string;
-    updatedAt: string;
-    user: SafeUser;
-  };
+export type SafeReview = Omit<Review, "createdAt" | "updatedAt"> & {
+  createdAt: string;
+  updatedAt: string;
+  user: SafeUser;
+};
 
 type ListingWithImages = Prisma.ListingGetPayload<{
   include: { images: true | Prisma.ImageFindManyArgs };
 }>;
 
-export type SafeListing = Omit<ListingWithImages, 'createdAt' | 'updatedAt'> & {
+export type SafeListing = Omit<ListingWithImages, "createdAt" | "updatedAt"> & {
   createdAt: string;
   updatedAt: string;
 };
 
 export type SafeReservation = Omit<
   Reservation,
-  'createdAt' | 'startDate' | 'endDate' | 'listing'
+  "createdAt" | "startDate" | "endDate" | "listing"
 > & {
   createdAt: string;
   startDate: string;
   endDate: string;
-  listing: SafeListing;
+  listing: SafeListing & { user: SafeUser };
 };
